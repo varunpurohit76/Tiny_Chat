fe5713c8e63b613cedff533915b53f35ea60b289
monicagangwar <monicagangwar@hotmail.com>
2016-06-13 23:04:26 +0530

    socket chat option

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,12 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create()
 
-gulp.task('watch',['browserSync'],function(){
-	gulp.watch('app/*.js',['scripts']);
-	gulp.watch('app/*.html',['views']);
-});
-
-gulp.task('browserSync',function(){
+gulp.task('serve',function(){
 	browserSync.init({
-		server : {
-			baseDir: 'app'
-		},
-	})
-});
-
-gulp.task('scripts',function(){
-	return gulp.src('app/*.js')
-	.pipe(browserSync.reload({
-		stream: true
-	}))
-});
-
-gulp.task('views',function(){
-	return gulp.src('app/*.html')
-	.pipe(browserSync.reload({
-		stream: true
-	}))
+		server:{
+			baseDir:"./"
+		}
+	});
+	gulp.watch("*.js",browserSync.reload);
+	gulp.watch("*.html",browserSync.reload);
 });
--- server.js
+++ server.js
@@ -3,11 +3,13 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+	res.sendfile('index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+	socket.on('chat message',function(msg){
+		console.log('message' + msg);
+	});
 });
 
 http.listen(3000, function(){
