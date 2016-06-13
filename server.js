29584e40c1ce1d0027e5b935979ca8c969571063
Varun Purohit <varunpurohit76@gmail.com>
2016-06-13 21:40:34 +0530

    socket server basic

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-var app = require('express');
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-});
+	console.log('user connected!');
+	socket.on('disconnect', function(){
+		console.log('user disconnected!');
+	})
+})
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
