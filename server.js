3ef7d60227c5776a19c42ce0798f864f91519e5d
monicagangwar <monicagangwar@hotmail.com>
2016-06-15 20:37:27 +0530

    added user nicknames

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ io.on('connection', function(socket){
 	socket.on('disconnect', function(){
 		console.log('user disconnected!');
 	})
-	socket.on('chat message', function(msg){
+	socket.on('chat message', function(msg,user){
 		console.log('message : ' + msg);
-		io.emit('chat message', msg);
+		io.emit('chat message', msg,user);
 	});
 });
 
