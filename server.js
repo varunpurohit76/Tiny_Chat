6eeef1f3fec3c2ca759daa8d6e40ece9e163f7b6
monicagangwar <monicagangwar@hotmail.com>
2016-06-15 19:48:44 +0530

    working mvp

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-	res.sendfile('index.html');
+	  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-	socket.on('chat message',function(msg){
-		console.log('message' + msg);
-	});
+	  console.log('a user connected');
+		socket.on('chat message',function(msg){
+			console.log(msg);
+			io.emit('chat message',msg);
+		});
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+	  console.log('listening on *:3000');
 });
