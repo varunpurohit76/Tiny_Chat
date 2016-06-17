fc945ea30441e030a936f8732ae86c6f08cda656
monicagangwar <monicagangwar@hotmail.com>
2016-06-17 15:14:35 +0530

    username added

--- server.js
+++ server.js
@@ -8,12 +8,9 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-	console.log('user connected!');
 	socket.on('disconnect', function(){
-		console.log('user disconnected!');
 	})
 	socket.on('chat message', function(msg,user){
-		console.log('message : ' + msg);
 		io.emit('chat message', msg,user);
 	});
 });
